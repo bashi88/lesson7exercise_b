f6fcaaabc9b1d3c7ddc7c6fe2fd47893a0efdc54
Barbara <barbasien@wp.pl>
2015-01-14 09:57:23 +0100

    small changes

--- Main.R
+++ Main.R
@@ -1,7 +1,7 @@
 # Team: ZeaPol   
 # Team Members: Roeland de Koning / Barbara Sienkiewicz    
 # Date: 12/01/2015       
-# Exercise 6
+# Exercise 7
 
 library(raster)
 library(rgeos)
@@ -16,10 +16,8 @@ load("data/GewataB7.rda")
 load("data/GewataB2.rda")
 load("data/GewataB3.rda")
 load("data/GewataB4.rda")
-
 load("data/vcfGewata.rda")
 load("data/trainingPoly.rda")
-
 load("data/lulcGewata.rda")
 load("data/LUTGewata.rda")
 
@@ -32,15 +30,14 @@ load("data/LUTGewata.rda")
 # bands and the VCF tree cover. What can you conclude from this/these plot(s)?
 
 
-#create the basic gewata raster and adjust it also produce the first required pairsplot
-
+# Create the basic gewata raster and adjust it also produce the first required pairsplot:
 gewata <- brick(GewataB1,GewataB2, GewataB3, GewataB4, GewataB5, GewataB7)
 names(gewata) <- c("band1", "band2", "band3", "band4",  "band5", "band7")
-
 gewata <- calc(gewata, fun=function(x) x / 10000)
 
 vcfGewata[vcfGewata > 100] <- NA
 plot(vcfGewata)
+
 names(vcfGewata) <- c("VCF")
 summary(vcfGewata)
 
@@ -52,13 +49,14 @@ pairs(gewatavcf)
 ######################################################################################
 ######################################################################################
 
-#optional data sources
+# optional data sources
 
-#create NDVI raster as option
+# create NDVI raster as option:
 
 ndvi <- overlay(GewataB4, GewataB3, fun=function(x,y){(x-y)/(x+y)})
 
-#create basic mask versions (forest) of basic gewata raster as option
+
+# create basic mask versions (forest) of basic gewata raster as option:
 
 lulc <- as.factor(lulcGewata)
 levels(lulc) <- LUTGewata
@@ -67,19 +65,19 @@ names(classes) <- LUTGewata$Class
 plot(classes, legend=FALSE)
 forest <- classes$forest
 forest[forest==0] <- NA
-plot(forest, col="dark green", legend=FALSE)
-gewataforest <- mask(gewatavcf,forest, filename = "output/LandSatBandsVCFForestRelationship", overwrite = T)
+plot(forest, col="dark green", legend = FALSE)
+gewataforest <- mask(gewatavcf, forest, filename = "output/LandSatBandsVCFForestRelationship", overwrite = T)
 plot(gewataforest)
 
 ############################################################################################
 ###########################################################################################
 
-#Part 2
+# Part 2
 # Create an lm() model and show a summary (e.g. using summary()) of the model object you created. 
 # Which predictors (bands) are probably most important in predicting tree cover?
 
-#create the polygon training classification sytem, data for the lm
 
+# Create the polygon training classification sytem, data for the lm:
 trainingPoly@data$Code <- as.numeric(trainingPoly@data$Class)
 classes <- rasterize(trainingPoly, ndvi, field='Code')
 names(classes) <- c("class")
@@ -87,14 +85,12 @@ cols <- c("orange", "dark green", "light blue")
 plot(classes, col=cols, legend=FALSE)
 legend("topright", legend=c("cropland", "forest", "wetland"), fill=cols, bg="white")
 
-#create a masked version of the training model
-
+# create a masked version of the training model:
 gewatatraining <- mask(gewatavcf, classes)
 gewatatraining <- addLayer(gewatatraining,classes)
 plot(gewatatraining)
 
-#create the gewataavf and gewatatraining data frame 
-
+# Create the gewataavf and gewatatraining data frame:
 gewatavcftable <- getValues(gewatavcf)
 gewatavcftable2 <- na.omit(gewatavcftable)
 gewatavcfDF <- as.data.frame(gewatavcftable2)
@@ -110,13 +106,18 @@ head(gewatavcfDF, n = 10)
 head(gewatatrainingDF, n = 10)
 
 # run the linear model on the gewatavcfDF data frame use most predicitve bands (3 and 7)
-
 lm.a1 <- lm(band3 ~ band7, data = gewatavcfDF)
 summary(lm.a1)
 
 
-modelRF <- randomForest(x=gewatatrainingDF[ ,c(3,6)], y=gewatatrainingDF$class,
-                        importance = TRUE)
+
+############################################################################################
+###########################################################################################
+
+# Part 3
+# Plot the predicted tree cover raster and compare with the original VCF raster.
+
+model.rf <- randomForest(x = gewatatrainingDF[ ,c(3,6)], y = gewatatrainingDF$class, importance = TRUE)
 
 predLC <- predict(gewatatraining, model=lm.a1, na.rm=TRUE)
 
@@ -130,3 +131,23 @@ plot(gewatalm)
 gewatalmforest <- mask(gewatalm,forest)
 plot(gewatalmforest)
 
+
+############################################################################################
+###########################################################################################
+
+# Part 4
+# compute the RMSE between your predicted and the actual tree cover values
+
+
+
+
+
+
+
+############################################################################################
+###########################################################################################
+
+# Part 5
+# Are the differences between the predicted and actual tree cover the same for all of the 3 classes we 
+# used for the random forest classfication? Using the training polygons from the random forest classification, 
+# calculate the RMSE separately for each of the classes and compare. Hint - see ?zonal().
